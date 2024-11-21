fa6c5abc020dce8f35175626204f65f227bbaad3
saikumarmadagoni <42109619+saikumarmadagoni@users.noreply.github.com>
2024-11-22 00:42:54 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -18,9 +18,9 @@ def sonarscan(){
        dir(env.svc_name){
     git branch:env.svc_branch ,url: "git@github.com:saikumarmadagoni/"+env.svc_name+".git" ,  credentialsId: "madagonitoken"
    }
-    sh('cd '+env.svc_name)
-     sh('ls -ltrh')     
+      dir(env.svc_name){   
      sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=spring-svc -Dsonar.projectName='spring-svc' -Dsonar.host.url=http://localhost:9000'
+      }
       }
   }
 
